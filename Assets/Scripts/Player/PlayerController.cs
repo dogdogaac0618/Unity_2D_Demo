e63cb78962e38ba4202543036725c2e9b7899247
dogdogaac0618 <3214833496@qq.com>
2026-04-24 20:12:49 +0800

    Update PlayerController.cs
    changed movespeed

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Move Settings")]
-    public float moveSpeed = 5f;
+    public float moveSpeed = 6f;
 
     [Header("Dash Settings")]
     public float dashSpeed = 12f;
